baaafd5a411e350f585622a564016c64eb54853a
sebacornnejo <scornejoguz@gmail.com>
2023-11-17 23:57:57 -0300

    message

--- SimpleDataVisualization_withPlotly/ExampleDataIris01.py
+++ SimpleDataVisualization_withPlotly/ExampleDataIris01.py
@@ -6,205 +6,344 @@ import statsmodels
 # Load the Iris dataset
 df = px.data.iris()
 
+# Map species names to the desired format
+species_mapping = {
+    "setosa": "Setosa",
+    "versicolor": "Versicolor",
+    "virginica": "Virginica",
+}
+df["species"] = df["species"].map(species_mapping)
+
 # -----------------------------------------------------------------------------------------#
 # Simple Scatterplot
-fig1 = px.scatter(df, x="sepal_width", y="sepal_length", 
-                  title='<b>Sepal Width vs Sepal Length<b>',
-                  labels={'sepal_width': 'Sepal Width', 'sepal_length': 'Sepal Length'},
-                  template='plotly_dark', color_discrete_sequence=['#6331C5'])
+# fig1 = px.scatter(
+#     df,
+#     x="sepal_width",
+#     y="sepal_length",
+#     title='<b style="font-size:24px>A Simple Scatterplot</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={"sepal_width": "Sepal Width", "sepal_length": "Sepal Length"},
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5"],
+# )
 
-# Customize the layout
-fig1.update_layout(font_family="Montserrat")
+# # Customize the layout
+# fig1.update_layout(
+#     xaxis_title="Sepal Width", yaxis_title="Sepal Length", font_family="Montserrat"
+# )
 
-fig1.show()
+# # Adjust point size fixedly
+# fig1.update_traces(marker=dict(size=12, line=dict(color="#F2F2F2", width=2)))
+
+# fig1.show()
 
 # Save the interactive HTML chart
-# fig1.write_html("scatterplot_sepal.html")
+# fig1.write_html(".\HTMLs\scatterplot_sepal.html")
 
 # -----------------------------------------------------------------------------------------#
 # Scatterplot with Color and Size
-fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", size="petal_length",
-                  title='<b>Sepal Width vs Sepal Length with Species and Petal Length<b>',
-                  labels={'sepal_width': 'Sepal Width', 'sepal_length': 'Sepal Length', 'petal_length': 'Petal Length'},
-                  template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig2 = px.scatter(
+#     df,
+#     x="sepal_width",
+#     y="sepal_length",
+#     color="species",
+#     size="petal_length",
+#     title='<b style="font-size:24px>A Simple Scatterplot with Color and Size</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={
+#         "sepal_width": "Sepal Width",
+#         "sepal_length": "Sepal Length",
+#         "petal_length": "Petal Length",
+#         "species": "Species",
+#     },
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5", "#3F7AD8", "#12BF80"],
+# )
 
-# Customize the layout
-fig2.update_layout(font_family="Montserrat")
+# fig2.update_traces(marker=dict(line=dict(color="#F2F2F2", width=1)))
 
-fig2.show()
+# # Customize the layout
+# fig2.update_layout(
+#     # xaxis_title="Sepal Width",
+#     # yaxis_title="Sepal Length",
+#     font_family="Montserrat",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=0.08, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
+
+# fig2.show()
 
 # Save the interactive HTML chart
-# fig2.write_html("scatterplot_species.html")
+# fig2.write_html(".\HTMLs\scatterplot_species.html")
 
 # -----------------------------------------------------------------------------------------#
 # 3D Scatterplot
-fig3 = px.scatter_3d(df, x="sepal_length", y="petal_length", z="petal_width", color="species", size="sepal_width",
-                     title='<b>3D Scatterplot of Sepal and Petal Dimensions<b>',
-                     labels={'sepal_length': 'Sepal Length', 'petal_length': 'Petal Length', 'petal_width': 'Petal Width'},
-                     template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig3 = px.scatter_3d(
+#     df,
+#     x="sepal_length",
+#     y="petal_length",
+#     z="petal_width",
+#     color="species",
+#     size="sepal_width",
+#     title='<b style="font-size:24px>A Simple 3D Scatterplot</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={
+#         "sepal_length": "Sepal Length",
+#         "petal_length": "Petal Length",
+#         "petal_width": "Petal Width",
+#         "species": "Species",
+#         "sepal_width": "Sepal Width",
+#     },
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5", "#3F7AD8", "#12BF80"],
+# )
 
-# Customize the layout
-fig3.update_layout(font_family="Montserrat")
+# fig3.update_traces(marker=dict(line=dict(color="#F2F2F2", width=1)))
 
-fig3.show()
+# # Customize the layout
+# fig3.update_layout(
+#     font_family="Montserrat",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=0.08, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
+
+# fig3.show()
 
 # Save the interactive HTML chart
-# fig3.write_html("scatterplot_3d.html")
+# fig3.write_html(".\HTMLs\scatterplot_3d.html")
 
 # -----------------------------------------------------------------------------------------#
 # Scatter Matrix
-fig4 = px.scatter_matrix(df, dimensions=["sepal_width", "sepal_length", "petal_width", "petal_length"], color="species",
-                        title='<b>Scatter Matrix of Sepal and Petal Dimensions<b>',
-                        template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig4 = px.scatter_matrix(
+#     df,
+#     dimensions=["sepal_width", "sepal_length", "petal_width", "petal_length"],
+#     color="species",
+#     title='<b style="font-size:24px>A Simple Scatter Matrix</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={
+#         "sepal_length": "Sepal Length",
+#         "petal_length": "Petal Length",
+#         "petal_width": "Petal Width",
+#         "species": "Species",
+#         "sepal_width": "Sepal Width",
+#     },
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5", "#3F7AD8", "#12BF80"],
+# )
 
-# Customize the layout
-fig4.update_layout(font_family="Montserrat")
+# # Customize the layout
+# fig4.update_layout(
+#     font_family="Montserrat",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=1.07, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
 
-fig4.show()
+# fig4.show()
 
-# Save the interactive HTML chart
-# fig4.write_html("scatter_matrix.html")
+# # Save the interactive HTML chart
+# fig4.write_html(".\HTMLs\scatter_matrix.html")
 
 # -----------------------------------------------------------------------------------------#
 # Boxplot
-fig5 = px.box(df, y="sepal_width", 
-              title='<b>Boxplot of Sepal Width<b>',
-              labels={'sepal_width': 'Sepal Width'},
-              template='plotly_dark', color_discrete_sequence=['#6331C5'])
+# fig5 = px.box(
+#     df,
+#     y="sepal_width",
+#     title='<b style="font-size:24px>A Simple Boxplot</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={"sepal_width": "Sepal Width"},
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5"],
+# )
 
-# Customize the layout
-fig5.update_layout(font_family="Montserrat")
+# # Customize the layout
+# fig5.update_layout(font_family="Montserrat")
 
-fig5.show()
+# fig5.show()
 
 # Save the interactive HTML chart
-# fig5.write_html("boxplot_sepal_width.html")
+# fig5.write_html(".\HTMLs\boxplot_sepal_width.html")
 
 # -----------------------------------------------------------------------------------------#
 # Boxplot Comparison
-fig6 = px.box(df, x="species", y="sepal_width", color="species",
-              title='<b>Boxplot Comparison of Sepal Width by Species<b>',
-              labels={'sepal_width': 'Sepal Width'},
-              template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig6 = px.box(
+#     df,
+#     x="species",
+#     y="sepal_width",
+#     color="species",
+#     title='<b style="font-size:24px>A Simple Boxplot Comparison</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={"sepal_width": "Sepal Width", "species": "Species"},
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5", "#3F7AD8", "#12BF80"],
+# )
 
-# Customize the layout
-fig6.update_layout(font_family="Montserrat")
+# # Customize the layout
+# fig6.update_layout(
+#     font_family="Montserrat",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=0.96, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
 
-fig6.show()
+# fig6.show()
 
 # Save the interactive HTML chart
-# fig6.write_html("boxplot_species_comparison.html")
+# fig6.write_html(".\HTMLs\boxplot_species_comparison.html")
 
 # -----------------------------------------------------------------------------------------#
 # Histogram
-fig7 = px.histogram(df, x="sepal_width", 
-                    title='<b>Histogram of Sepal Width<b>',
-                    labels={'sepal_width': 'Sepal Width'},
-                    template='plotly_dark', color_discrete_sequence=['#6331C5'])
+# import numpy as np
 
-# Customize the layout
-fig7.update_layout(font_family="Montserrat")
+# # Calculate percentage bins from histogram using numpy
+# # This is because Plotly does not have the ability to display "count" and "percent" at the same time in "hover"
+# bin = 13;
+# hist, _ = np.histogram(df["sepal_width"], bins=bin, density=False)
+# hist_percent = (hist / len(df)) * 100
 
-fig7.show()
+# fig7 = px.histogram(
+#     df,
+#     x="sepal_width",
+#     nbins=bin,
+#     title='<b style="font-size:24px>A Simple Histogram</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={"sepal_width": "Sepal Width"},
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5"],
+# )
+
+# fig7.update_traces(
+#     marker=dict(line=dict(color="#F2F2F2", width=0.5)),
+#     hovertemplate="Bin: %{x}<br>Count: %{y}<br>Percent: %{customdata:.2f}%",
+#     customdata=hist_percent,
+# )
+
+# # Customize the layout
+# fig7.update_layout(font_family="Montserrat")
+
+# fig7.show()
 
 # Save the interactive HTML chart
-# fig7.write_html("histogram_sepal_width.html")
+# fig7.write_html(".\HTMLs\histogram_sepal_width.html")
 
 # -----------------------------------------------------------------------------------------#
 # Multiple Histograms
-fig8 = px.histogram(df, x="sepal_width", color="species", 
-                    title='<b>Overlayed Histograms of Sepal Width by Species<b>',
-                    labels={'sepal_width': 'Sepal Width'},
-                    template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig8 = px.histogram(
+#     df,
+#     x="sepal_width",
+#     color="species",
+#     nbins=10,
+#     title='<b style="font-size:24px>A Simple Multiple Histogram</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={"sepal_width": "Sepal Width", "species": "Species"},
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5", "#3F7AD8", "#12BF80"],
+# )
 
-# Customize the layout
-fig8.update_layout(font_family="Montserrat", barmode='overlay')
-fig8.update_traces(opacity=0.75)
+# fig8.update_traces(marker=dict(line=dict(color="#F2F2F2", width=0.5)),hovertemplate='Bin: %{x}<br>Count: %{y}')
 
-fig8.show()
+# # Customize the layout
+# fig8.update_layout(
+#     font_family="Montserrat",
+#     barmode="group",
+#     yaxis_title=" ",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=0.98, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
+
+# fig8.show()
 
 # Save the interactive HTML chart
-# fig8.write_html("histogram_species_overlay.html")
+# fig8.write_html(".\HTMLs\histogram_species_overlay.html")
 
 # -----------------------------------------------------------------------------------------#
 # Histogram for General Counting
-fig9 = px.histogram(df, x="species", color="sepal_width", histfunc="count", height=300, 
-                    title='<b>Histogram Chart for General Counting<b>',
-                    labels={'species': 'Species'},
-                    template='plotly_dark')
+# color_palette_cpt = [
+#     "#EAF759","#F1E44F","#F7D246","#FABF3F","#FCAE3C","#FB9C3E","#F78C45","#F07F50","#E5745E","#D76C6B","#C76676","#B7617F","#A65C86","#96578A","#87518E","#784B91","#694496","#593D9B","#4736A0","#303399","#1A347F","#0C3062","#062B49","#042333"
+# ]
 
-# Customize the layout
-fig9.update_layout(font_family="Montserrat")
+# df_sorted = df.sort_values(by='sepal_width', ascending=False)
 
-fig9.show()
+# fig9 = px.histogram(df_sorted, x="species", color="sepal_width", histnorm="percent", height=400,
+#                     title='<b style="font-size:24px>A Simple Histogram Chart for General Percentage</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#                     labels={'species': 'Species',"sepal_width": "Sepal Width"},
+#                     template='plotly_dark',color_discrete_sequence=color_palette_cpt,
+#                    )
 
-# Save the interactive HTML chart
-# fig9.write_html("histogram_general_count.html")
+# # Customize the layout
+# fig9.update_layout(height=600, width=1080,font_family="Montserrat",showlegend=True,
+#     yaxis_title=" ",
+#     legend_title="<b>Sepal Width:<b>",
+#     legend=dict(bgcolor="rgba(255, 255, 255, 0.5)", traceorder='reversed'),
+#     legend_font_color="#262626",)
 
-# -----------------------------------------------------------------------------------------#
-# Bar Chart
-fig10 = px.bar(df, x="species", 
-               title='<b>Bar Chart of Species<b>',
-               labels={'species': 'Species'},
-               template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig9.update_traces(hovertemplate='<b>Percent</b>: %{y:.2f}%')
 
-# Customize the layout
-fig10.update_layout(font_family="Montserrat")
-
-fig10.show()
+# fig9.show()
 
 # Save the interactive HTML chart
-# fig10.write_html("bar_chart_species.html")
-
-# -----------------------------------------------------------------------------------------#
-# Bar Chart with Values
-fig11 = px.bar(df, x="species", y="sepal_length", 
-               title='<b>Bar Chart of Sepal Length by Species<b>',
-               labels={'species': 'Species', 'sepal_length': 'Sepal Length'},
-               template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
-
-# Customize the layout
-fig11.update_layout(font_family="Montserrat")
-
-fig11.show()
-
-# Save the interactive HTML chart
-# fig11.write_html("bar_chart_sepal_length.html")
+# fig9.write_html(".\HTMLs\histogram_general_count.html")
 
 # -----------------------------------------------------------------------------------------#
 # Scatterplot with Regression Lines
-fig12 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", trendline="ols", 
-                   title='<b>Scatterplot with Regression Lines<b>',
-                   labels={'sepal_width': 'Sepal Width', 'sepal_length': 'Sepal Length'},
-                   template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig10 = px.scatter(
+#     df,
+#     x="sepal_width",
+#     y="sepal_length",
+#     color="species",
+#     trendline="ols",
+#     title='<b style="font-size:24px>A Simple Scatterplot with Regression Lines</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#     labels={
+#         "sepal_width": "Sepal Width",
+#         "sepal_length": "Sepal Length",
+#         "species": "Species",
+#     },
+#     template="plotly_dark",
+#     color_discrete_sequence=["#6331C5", "#3F7AD8", "#12BF80"],
+# )
 
-# Customize the layout
-fig12.update_layout(font_family="Montserrat")
+# fig10.update_traces(marker=dict(size=10, line=dict(color="#F2F2F2", width=0.5)))
+# # Customize the layout
+# fig10.update_layout(
+#     font_family="Montserrat",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=0.08, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
 
-fig12.show()
+# fig10.show()
 
-# Save the interactive HTML chart
-# fig12.write_html("scatterplot_regression.html")
+# # Save the interactive HTML chart
+# fig10.write_html(".\HTMLs\scatterplot_regression.html")
 
 # -----------------------------------------------------------------------------------------#
 # Scatterplot with Marginal Plots
-fig13 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", marginal_y="box", marginal_x="box", 
-                   trendline="ols", title='<b>Scatterplot with Marginal Plots<b>',
-                   template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
+# fig13 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", marginal_y="box", marginal_x="box", 
+#                    trendline="ols", title='<b style="font-size:24px>A Simple Scatterplot with Margin Plots</b><br><span style="font-size:14px;">from Iris Dataset</span>',
+#                    labels={"sepal_width": "Sepal Width", "sepal_length": "Sepal Length", "species": "Species"},
+#                    template='plotly_dark', color_discrete_sequence=['#6331C5', '#3F7AD8', '#12BF80'])
 
-# Customize the layout
-fig13.update_layout(font_family="Montserrat")
+# # Customize the layout
+# fig13.update_traces(marker=dict(size=10, line=dict(color="#F2F2F2", width=0.5)))
+# # Customize the layout
+# fig13.update_layout(
+#     font_family="Montserrat",
+#     showlegend=True,
+#     legend_title="<b>Species:<b>",
+#     legend=dict(x=0.74, y=0.98, bgcolor="rgba(255, 255, 255, 0.5)", orientation="h"),
+#     legend_font_color="#262626",
+# )
 
-fig13.show()
+# fig13.show()
 
 # Save the interactive HTML chart
-# fig13.write_html("scatterplot_marginal_plots.html")
+# fig13.write_html(".\HTMLs\scatterplot_marginal_plots.html")
 
 # -----------------------------------------------------------------------------------------#
 # Create a Dashboard with make_subplots
 from plotly.subplots import make_subplots
 import screeninfo
-
 
 fig14 = make_subplots(rows=4, cols=4, subplot_titles=['Sepal Width vs Sepal Length', 'Sepal Width vs Sepal Length with Species and Petal Length',
                                                       '3D Scatterplot of Sepal and Petal Dimensions', 'Scatter Matrix of Sepal and Petal Dimensions',
